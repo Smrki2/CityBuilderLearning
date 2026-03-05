92a9e70f6674bb31de0fbae0b3761d4950826e6d
Smrki2 <urosmarkovic2104@gmail.com>
2026-03-05 23:36:08 +0100

    camera movement and building placement done

--- Assets/_Project/Scripts/Core/CameraController.cs
+++ Assets/_Project/Scripts/Core/CameraController.cs
@@ -3,9 +3,9 @@ using UnityEngine.InputSystem;
 
 public class CameraController : MonoBehaviour
 {
-    [SerializeField] private Camera camera;    
-    [SerializeField] InputAction cameraMovementAction;
-    [SerializeField] InputAction cameraZoomAction;
+    private Camera camera;
+    [SerializeField] InputActionReference cameraMovementAction;
+    [SerializeField] InputActionReference cameraZoomAction;
 
     [SerializeField] float movementSpeed = 1f;
     [SerializeField] float zoomSpeed = 1f;
@@ -14,22 +14,23 @@ public class CameraController : MonoBehaviour
     Vector2 cameraZoom;
     private void Awake()
     {
+        camera = GetComponent<Camera>();
         if (cameraMovementAction != null && cameraZoomAction != null)
         {
-            cameraMovementAction.Enable();
-            cameraZoomAction.Enable();
+            cameraMovementAction.action.Enable();
+            cameraZoomAction.action.Enable();
         }
     }
 
     private void Update()
     {
-        cameraMovement = cameraMovementAction.ReadValue<Vector2>();
-        cameraZoom = cameraZoomAction.ReadValue<Vector2>();
+        cameraMovement = cameraMovementAction.action.ReadValue<Vector2>();
+        cameraZoom = cameraZoomAction.action.ReadValue<Vector2>();
 
         Vector3 tempCamMov = new Vector3(cameraMovement.x, 0, cameraMovement.y);
         camera.transform.position += tempCamMov * movementSpeed * Time.deltaTime;
 
         Vector3 tempCamZoom = new Vector3(0, cameraZoom.y, 0);
-        camera.transform.position += tempCamZoom * zoomSpeed * Time.deltaTime;
+        camera.transform.position -= tempCamZoom * zoomSpeed * Time.deltaTime;
     }
 }
--- Assets/_Project/Scripts/Grid/BuildingPlacer.cs
+++ Assets/_Project/Scripts/Grid/BuildingPlacer.cs
@@ -3,18 +3,19 @@ using UnityEngine.InputSystem;
 
 public class BuildingPlacer : MonoBehaviour
 {
-    [SerializeField] private Camera camera;
+    private Camera camera;
     [SerializeField] private GridSystem gridSystem;
-    [SerializeField] private InputAction placeBuildingAction;
+    [SerializeField] private InputActionReference placeBuildingAction;
     [SerializeField] private BuildingDataSO prefab;
 
     private GameObject placedBuilding;
     RaycastHit hit;
     private void Awake()
     {
-        placeBuildingAction.Enable();
+        camera = GetComponent<Camera>();
+        placeBuildingAction.action.Enable();
 
-        placeBuildingAction.performed += OnPlaceBuildingPerformed;
+        placeBuildingAction.action.performed += OnPlaceBuildingPerformed;
     }
 
     private void OnPlaceBuildingPerformed(InputAction.CallbackContext context)
@@ -23,15 +24,15 @@ public class BuildingPlacer : MonoBehaviour
         if (Physics.Raycast(ray, out hit))
         {
             Vector2Int pos = new Vector2Int((int)Mathf.Floor(hit.point.x), (int)Mathf.Floor(hit.point.z));
-
-            if(!gridSystem.GetCellState(pos))
-            {
-                Vector3 place = new Vector3(gridSystem.GetCell(pos).cellPosition.x, 0, gridSystem.GetCell(pos).cellPosition.y);
-                placedBuilding = Instantiate(prefab.buildingPrefab, place, prefab.buildingPrefab.transform.rotation);
-                gridSystem.GetCell(pos).isUsed = true;
-                gridSystem.GetCell(pos).building = placedBuilding;
-                gridSystem.GetCell(pos).buildingType = prefab;
-            }
+            if(pos.x >= 0 && pos.x < gridSystem.gridSize.x && pos.y >= 0 && pos.y < gridSystem.gridSize.y)
+                if (!gridSystem.GetCellState(pos))
+                {
+                    Vector3 place = new Vector3(gridSystem.GetCell(pos).cellPosition.x, prefab.buildingPrefab.GetComponent<Renderer>().bounds.size.y / 2, gridSystem.GetCell(pos).cellPosition.y);
+                    placedBuilding = Instantiate(prefab.buildingPrefab, place, prefab.buildingPrefab.transform.rotation);
+                    gridSystem.GetCell(pos).isUsed = true;
+                    gridSystem.GetCell(pos).building = placedBuilding;
+                    gridSystem.GetCell(pos).buildingType = prefab;
+                }
         }
     }
 }
