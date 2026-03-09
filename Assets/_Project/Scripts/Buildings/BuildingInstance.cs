5e8db8d9b5ec0f6795d8c56f5ce127d5fe412bfe
Smrki2 <urosmarkovic2104@gmail.com>
2026-03-09 20:00:26 +0100

    added building demolish feature

---
+++ Assets/_Project/Scripts/Buildings/BuildingInstance.cs
@@ -0,0 +1,6 @@
+using UnityEngine;
+
+public class BuildingInstance : MonoBehaviour
+{
+    public Vector2Int GridPosition { get; set; }
+}
--- Assets/_Project/Scripts/Grid/BuildingPlacer.cs
+++ Assets/_Project/Scripts/Grid/BuildingPlacer.cs
@@ -12,6 +12,7 @@ public class BuildingPlacer : MonoBehaviour
     private Ray ray;
     private GameObject placedBuilding;
     private RaycastHit hit;
+    private bool demolishMode = false;
     private void Awake()
     {
         camera = GetComponent<Camera>();
@@ -22,25 +23,36 @@ public class BuildingPlacer : MonoBehaviour
 
     private void OnPlaceBuildingPerformed(InputAction.CallbackContext context)
     {
-        if (selectedBuilding == null)
-            return;
         ray = camera.ScreenPointToRay(Mouse.current.position.ReadValue());
         if (Physics.Raycast(ray, out hit,Mathf.Infinity,layerMask))
         {
-            if (hit.collider.gameObject.layer == LayerMask.NameToLayer("Ground"))
+            if (hit.collider.gameObject.layer == LayerMask.NameToLayer("Ground") && !demolishMode)
             {
+                if (selectedBuilding == null)
+                    return;
                 Vector2Int pos = new Vector2Int((int)Mathf.Floor(hit.point.x), (int)Mathf.Floor(hit.point.z));
                 if (pos.x >= 0 && pos.x < gridSystem.gridSize.x && pos.y >= 0 && pos.y < gridSystem.gridSize.y)
                     if (!gridSystem.GetCellState(pos))
                     {
                         Vector3 place = new Vector3(gridSystem.GetCell(pos).cellPosition.x + 0.5f, selectedBuilding.buildingPrefab.GetComponent<Renderer>().bounds.size.y / 2, gridSystem.GetCell(pos).cellPosition.y + 0.5f);
                         placedBuilding = Instantiate(selectedBuilding.buildingPrefab, place, selectedBuilding.buildingPrefab.transform.rotation);
+                        placedBuilding.GetComponent<BuildingInstance>().GridPosition = pos;
                         gridSystem.GetCell(pos).isUsed = true;
                         gridSystem.GetCell(pos).building = placedBuilding;
                         gridSystem.GetCell(pos).buildingType = selectedBuilding;
                     }
-                Debug.Log("Hit point: " + hit.point);
-                Debug.Log("Grid pos: " + pos);
+            }
+            else if(hit.collider.gameObject.layer == LayerMask.NameToLayer("Building") && demolishMode)
+            {
+                BuildingInstance instance = hit.collider.gameObject.GetComponent<BuildingInstance>();
+                if (instance == null)
+                {
+                    Debug.Log(instance);
+                    Debug.Log(hit.collider.gameObject.name);
+                    return; 
+                }
+                gridSystem.GetCell(instance.GridPosition).isUsed = false;
+                Destroy(hit.collider.gameObject);
             }
         }
     }
@@ -54,7 +66,12 @@ public class BuildingPlacer : MonoBehaviour
 
     public void SelectBuilding(BuildingDataSO clickedBuilding)
     {
+        demolishMode = false;
         selectedBuilding = clickedBuilding;
     }
 
+    public void SetDemolishMode()
+    {
+        demolishMode = true;
+    }
 }
